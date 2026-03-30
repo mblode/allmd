b87e7edf1d26b497a6b422ed56c501727d092fe1
Matthew Blode <matthew@linktr.ee>
2026-03-30 12:14:04 +1100

    Save

--- apps/docs/app/llms-full.txt/route.ts
+++
@@ -1,9 +0,0 @@
-import { getLLMText, source } from "@/lib/source";
-
-export const revalidate = false;
-
-export const GET = async () => {
-  const scanned = await Promise.all(source.getPages().map(getLLMText));
-
-  return new Response(scanned.join("\n\n"));
-};
